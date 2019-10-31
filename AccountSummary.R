417d412d6dd4cb66c59300c1c957b375fef62249
John Akwei <johnakwei1@gmail.com>
2019-10-31 07:20:28 -0400

    Update AccountSummary.R

--- AccountSummary.R
+++ AccountSummary.R
@@ -11,15 +11,7 @@
 # and the number of notes in the portfolio.
 
 Accounts <- function(searchURL){
-  searchURL <- paste0(searchURL, "/", "summary", sep="")
-  
-  r <- GET(searchURL,
-           add_headers("Accept": "application/json",
-                       "Authorization": "bearer",
-                       oauthToken.access_token))
-  
-  if(httr::status_code(r)!=200){
-    stop("Prosper API request failed")}
+  r <- GET("https://api.prosper.com/v1/summary")
   
   parsed <- fromJSON(content(r, "text", encoding="UTF-8"),
                      simplifyVector = TRUE)
@@ -27,13 +19,11 @@ Accounts <- function(searchURL){
   if (httr::http_error(r)) {
     stop(sprintf("Prosper API request failed [%s]\n%s\n<%s>",
                  status_code(r), parsed$message, parsed$documentation_url
-    ), call. = FALSE)
-  }
+    ), call. = FALSE)}
   
   parsed <- {y <- unlist(parsed)
   data.frame("Field"= names(y), "Value"= as.character(y),
-             stringsAsFactors = F, row.names = NULL)
-  }
+             stringsAsFactors = F, row.names = NULL)}
   
   structure(list(content = parsed, response = r),
             class = "Prosper_API")
@@ -41,7 +31,5 @@ Accounts <- function(searchURL){
 
 # Example
 #
-# Prosper_Base_Address <- "https://api.prosper.com/v1"
-# clientID <- "b7cdc9351d6d41c984bca165f00dd52e"
-# auth <- "1098273"
-# Accounts <- Accounts(Prosper_Base_Address, clientid, auth)
+# Accounts <- Accounts()
+# print(Accounts)
