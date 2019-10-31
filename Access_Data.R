c949a7994912d6328a95503da784cc8f78bf6ab7
John Akwei <johnakwei1@gmail.com>
2019-10-31 13:28:00 -0400

    Update Access_Data.R

--- Access_Data.R
+++ Access_Data.R
@@ -8,18 +8,16 @@
 
 # AccessData function
 Access_Data <- function(investorID, investorSecret,
-                        prosperUsername, prosperPassword){
-  data <- toJSON(data.frame(list(
-    grant_type='password',
-    client_id=investorID,
-    client_secret=investorSecret,
-    username=prosperUsername,
-    password=prosperPassword
-  )))
+                       prosperUsername, prosperPassword){
+  data <-  list(grant_type='password',
+                client_id=investorID,
+                client_secret=investorSecret,
+                username=prosperUsername,
+                password=prosperPassword)
   data
 }
 
 # Example
-# payload <- Access_Data(clientid, clientsecret,clientusername,
-#                        clientpassword)
+# payload <- Access_Data(client_id, client_secret,username,
+                       # password)
 # payload
