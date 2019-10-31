dd827b40c362f2ba393c9eae5bd8309b986a8cba
John Akwei <johnakwei1@gmail.com>
2019-10-31 07:28:15 -0400

    Update Authorization.R

--- Authorization.R
+++ Authorization.R
@@ -6,18 +6,17 @@
 ##################################################
 ##################################################
 
-# A wrapper to obtain the authorization key for
-# access to the Prosper API.
-# "authURL", the prosper.com authorization url
-# "AuthHeader", authorization header from "AuthHeader()"
-# "data", a list created with the "AuthData()"
-# "auth", a prosper authorization key from "RequestToken.R"
+# A function to obtain the authorization key for access to the Prosper API.
+# "authKey", a prosper authorization key from "RequestToken.R"
 
 Authorization <- function(authKey){
-  response <- POST("https://api.prosper.com/v1/", encode="json",
-                   add_headers("Accept": "application/json",
-                               "Authorization": "bearer",
-                               oauthToken.access_token))
+  response <- POST(paste0("https://api.prosper.com/v1/", authKey, sep = ""))
+  
+  if (httr::http_error(response)) {
+    stop(sprintf("Authorization failed [%s]\n%s\n<%s>",
+                 status_code(response), parsed$message, parsed$documentation_url
+    ), call. = FALSE)}
+  
   response
 }
 
